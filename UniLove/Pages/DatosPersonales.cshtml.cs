b5c5918f6901ea6f82258fcfaa3d8b6347487718
Juan Adalberto Saldivar Rodriguez <jsaldivarre@uanl.edu.mx>
2024-10-31 22:36:08 -0600

    Ingreso de pagina de registro datos personales

---
+++ UniLove/Pages/DatosPersonales.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UniLove.Pages
+{
+    public class DatosPersonalesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
